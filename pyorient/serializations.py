696e14ff266d9268b1a95f06199f219fec2a36aa
Marc Auberer <marc.auberer@chillibits.com>
2020-08-11 12:19:19 +0200

    Fix import error

--- pyorient/serializations.py
+++ pyorient/serializations.py
@@ -17,7 +17,7 @@ __author__ = 'mogui <mogui83@gmail.com>, Marc Auberer <marc.auberer@sap.com>'
 #   limitations under the License.
 
 from .exceptions import PyOrientBadMethodCallException, PyOrientInvalidSerializationModeException
-from serializer import OrientSerializationCSV, OrientSerializationBinary
+from .serializer import OrientSerializationCSV, OrientSerializationBinary
 
 # Try to import pyorient_native. The binary protocol only can be used only pyorient_native is available
 try:
