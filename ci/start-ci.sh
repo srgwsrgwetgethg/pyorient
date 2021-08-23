5e31d3de887234cbc220a9bcf7b9c33d8762d21d
Bruce Schultz <bschultz013@gmail.com>
2021-08-23 15:40:46 +0200

    ci: add custom jdk script

--- ci/start-ci.sh
+++ ci/start-ci.sh
@@ -32,10 +32,19 @@ ODB_LAUNCHER_SYML="${CI_DIR}/orientdb_current/bin/server.sh"
 
 echo "=== Initializing CI environment ==="
 
-if [ -d "/usr/lib/jvm/java-8-oracle/jre" ]; then
-    # Force maven on Travis to use JAVA 8
-    export JAVA_HOME="/usr/lib/jvm/java-8-oracle/jre"
-fi
+# show current JAVA_HOME and java version
+echo "Current JAVA_HOME: $JAVA_HOME"
+echo "Current java -version:"
+java -version
+
+# install Java 8
+sudo add-apt-repository -y ppa:openjdk-r/ppa
+sudo apt-get -qq update
+sudo apt-get install -y openjdk-8-jdk --no-install-recommends
+sudo update-java-alternatives -s java-1.8.0-openjdk-amd64
+
+# change JAVA_HOME to Java 8
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 
 echo `java -version`
 echo `javac -version`
