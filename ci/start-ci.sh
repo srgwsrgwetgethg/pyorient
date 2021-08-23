033a37fa87d12eb52fba81d759320b41bb367748
Bruce Schultz <bschultz013@gmail.com>
2021-08-23 15:46:57 +0200

    ci: try openjdk11

--- ci/start-ci.sh
+++ ci/start-ci.sh
@@ -37,16 +37,16 @@ echo "Current JAVA_HOME: $JAVA_HOME"
 echo "Current java -version:"
 java -version
 
-# install Java 8
-sudo add-apt-repository -y ppa:openjdk-r/ppa
-sudo apt-get -qq update
-sudo apt-get install -y openjdk-8-jdk --no-install-recommends
-sudo update-java-alternatives -s java-1.8.0-openjdk-amd64
-
-# change JAVA_HOME to Java 8
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-
-echo `java -version`
+## install Java 8
+#sudo add-apt-repository -y ppa:openjdk-r/ppa
+#sudo apt-get -qq update
+#sudo apt-get install -y openjdk-8-jdk --no-install-recommends
+#sudo update-java-alternatives -s java-1.8.0-openjdk-amd64
+#
+## change JAVA_HOME to Java 8
+#export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+#
+#echo `java -version`
 echo `javac -version`
 echo `mvn -version`
 
